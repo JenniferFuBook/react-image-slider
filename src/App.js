9934c57e5e001e1511b77fec052c5881a9a9a03d
Jennifer Fu <jenniferfu@Jennifers-MacBook-Pro.local>
2024-10-14 14:17:54 -0700

    Format

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { ImageSlider } from './components/image-slider/ImageSlider';
 function App() {
   return (
     <div>
-      <ImageSlider url="https://picsum.photos/v2/list/" limit={5}/>
-  </div>);
+      <ImageSlider url="https://picsum.photos/v2/list/" limit={5} />
+    </div>);
 }
 
 export default App;
